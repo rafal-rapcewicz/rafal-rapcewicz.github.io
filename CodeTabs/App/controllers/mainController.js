0e61eadfb9aa890a9a39361d8e25ddce4cd062e7
Rafał Rapcewicz <rrapcewicz@objectivity.co.uk>
2015-12-08 12:20:10 +0100

    try with external templates

--- CodeTabs/App/controllers/mainController.js
+++ CodeTabs/App/controllers/mainController.js
@@ -1,23 +1,53 @@
 ﻿(function () {
-    'use strict';
+    "use strict";
 
     function MainController() {
         this.code = null;
 
         this.init = function () {
             this.code = [
-              { language: 'html', source: '<p>Hello World!</p>' },
-              { language: 'css', source: 'p:after { content: "Hello World"; }' },
-              { title: 'JS', language: 'javascript', source: 'alert(\'Hello World!\');' },
-              { language: 'php', source: '<!--?php echo="Hello World!" ?-->' },
-              { language: 'java', source: 'System.out.println("Hello World!");' },
-              { title: 'C#', language: 'csharp', source: 'Console.WriteLine("Hello World!");' }
+                {
+                    language: "html",
+                    source:
+                        [
+                            '<p>Hello World!</p>',
+                            '<p>Hello World2!</p>'
+                        ].join('\n')
+                },
+                {
+                    language: "css",
+                    source: 'p:after { content: "Hello World"; }'
+                },
+                {
+                    title: "JS",
+                    language: "javascript",
+                    source: "alert('Hello World!');"
+                },
+                {
+                    language: "php",
+                    source: '<!--?php echo="Hello World!" ?-->'
+                },
+                {
+                    language: "java",
+                    source: 'System.out.println("Hello World!");'
+                },
+                {
+                    title: "C#",
+                    language: "csharp",
+                    source:
+                        [
+                            'Console.WriteLine("Hello World!")',
+                            'for(int i = 1; i < 10; i++',
+                            '{',
+                            '   Console.WriteLine(i);',
+                            '}'
+                        ].join('\n')
+                }
             ];
-        }
-
+        };
         this.init();
     }
 
-    angular.module('app').controller('mainController', MainController);
+    angular.module("app").controller("mainController", MainController);
 
 })();
