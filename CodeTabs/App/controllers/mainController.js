17cef50d889cb0dda3ae54dd6be10f09d6ee7095
rafal-rapcewicz <rafal.rapcewicz@gmail.com>
2015-12-08 21:08:05 +0100

    Update mainController.js

--- CodeTabs/App/controllers/mainController.js
+++ CodeTabs/App/controllers/mainController.js
@@ -7,28 +7,28 @@
         this.init = function () {
             this.code = [
                 {
-                    url: '/App/templates/html1.html',
+                    url: 'App/templates/html1.html',
                     language: "html"
                 },
                 {
-                    url: '/App/templates/css1.html',
+                    url: 'App/templates/css1.html',
                     language: "css"
                 },
                 {
-                    url: '/App/templates/javascript1.html',
+                    url: 'App/templates/javascript1.html',
                     title: "JS",
                     language: "javascript"
                 },
                 {
-                    url: '/App/templates/php1.html',
+                    url: 'App/templates/php1.html',
                     language: "php"
                 },
                 {
-                    url: '/App/templates/java1.html',
+                    url: 'App/templates/java1.html',
                     language: "java"
                 },
                 {
-                    url: '/App/templates/csharp1.html',
+                    url: 'App/templates/csharp1.html',
                     title: "C#",
                     language: "csharp"
                 }
